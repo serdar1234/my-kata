44fdaf6b661623416d243982a9087a0aca929b62
serdar1234 <serdarabad@gmail.com>
2024-11-16 15:06:52 +0300

    Improved js. added swiper links

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,46 @@
-const brands__list = document.querySelector('.brands__list');
-const allListItems = brands__list.querySelectorAll('.brands__item');
-const brands__button = document.querySelector('.brands__button');
-const brand__text = brands__button.querySelector('.brands__button-text');
-const brands__arrowIcon = brands__button.querySelector('.brands__arrow-icon');
+// Brand icons
+const brands__list = document.querySelector(".brands__list");
+const allListItems = brands__list.querySelectorAll(".brands__item");
 
-brands__button.addEventListener('click', () => {
-  if (brand__text.textContent !== "Скрыть") {
-    brand__text.textContent = "Скрыть";
-    brands__list.classList.add('brands__list--show-brands');
-    brands__arrowIcon.classList.add('brands__arrow-icon--turned');
-  } else {
-    brand__text.textContent = "Показать все";
-    brands__list.classList.remove('brands__list--show-brands');
-    brands__arrowIcon.classList.remove('brands__arrow-icon--turned');
-  }
-})
+// Toggle button and its elements
+const brands__button = document.querySelector(".brands__button");
+const brand__text = brands__button.querySelector(".brands__button-text");
+const brands__arrowIcon = brands__button.querySelector(".brands__arrow-icon");
+
+brands__button.addEventListener("click", () => {
+  // If all brand items are displayed == true
+  let allItemsDisplayed =
+    brand__text.textContent === "Показать все" ? true : false;
+  // Change the text  
+  allItemsDisplayed
+    ? (brand__text.textContent = "Скрыть")
+    : (brand__text.textContent = "Показать все");
+  
+  brands__list.classList.toggle("brands__list--show-brands");
+  brands__arrowIcon.classList.toggle("brands__arrow-icon--turned");
+});
+
+
+// **************** Swiper *************
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
